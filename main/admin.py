0128fd9532027f67fa79c5b62a59fa7ef04d86ac
pvlbrzn <pavelberezan1998@gmail.com>
2025-02-16 19:53:38 +0300

    configured the admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,37 @@
+import csv
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Post, Movie
 
 
 admin.site.register(Post)
-admin.site.register(Movie)
 
 
+# Экспорт фильмов в CSV
+@admin.action(description='Экспорт выбранных фильмов в CSV')
+def export_movies_csv(modeladmin, request, queryset):
+    meta = modeladmin.model._meta
+    field_names = [field.name for field in meta.fields]
 
+    # Настраиваем HTTP-ответ с типом CSV
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename={meta}.csv'
+    writer = csv.writer(response)
+
+    # Записываем заголовок CSV
+    writer.writerow(field_names)
+    # Записываем данные по каждой выбранной записи
+    for obj in queryset:
+        row = [getattr(obj, field) for field in field_names]
+        writer.writerow(row)
+    return response
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name', 'price')
+    list_filter = ('price',)
+    actions = [export_movies_csv]
+
+
+admin.site.register(Movie, MovieAdmin)
--- main/models.py
+++ main/models.py
@@ -7,6 +7,10 @@ class Post(models.Model):
     author = models.CharField(max_length=100)
     post_time = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
     def __str__(self):
         return self.title
 
@@ -15,6 +19,10 @@ class Movie(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название фильма")
     price = models.CharField(max_length=50, verbose_name="Цена билета")
     image_url = models.URLField(max_length=500, verbose_name="Картинка фильма", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
 
     def __str__(self):
         return f"{self.name} - {self.price}"
